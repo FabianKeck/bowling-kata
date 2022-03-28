486aab30b2da8b1310a0f107c0956590aeae711c
Fabian Keck <fabian.keck@posteo.de>
2022-03-28 13:14:02 +0200

    introduce Frames

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,6 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    private class Frame {
+        private final int first;
+        private final int second;
+
+        public Frame(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public boolean isSpare() {
+            return first + second == 10;
+        }
+
+        public int getFirst() {
+            return first;
+        }
+    }
     private final List<Integer> rolls = new ArrayList<>();
 
     public void roll(int pins) {
@@ -10,12 +27,26 @@ public class Game {
 
     public int getScore() {
         int sum = rolls.stream().mapToInt(i -> i).sum();
-        int bonus = 0;
+        List<Frame> frames = extractFrames();
+        int bonus = getBonus(frames);
+        return sum + bonus;
+    }
+
+    private List<Frame> extractFrames() {
+        List<Frame> frames = new ArrayList<>();
         for (int i = 2; i < rolls.size(); i += 2) {
-            if (rolls.get(i - 1) + rolls.get(i - 2) == 10) {
-                bonus += rolls.get(i);
+            frames.add(new Frame(rolls.get(i - 2), rolls.get(i - 1)));
+        }
+        return frames;
+    }
+
+    private int getBonus(List<Frame> frames) {
+        int bonus = 0;
+        for (int i = 0; i < frames.size(); i++) {
+            if (frames.get(i).isSpare()) {
+                bonus += frames.get(i + 1).getFirst();
             }
         }
-        return sum + bonus;
+        return bonus;
     }
 }
